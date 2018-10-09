9904f93931b295aad47636310e286dd00a01595b
Alexander Carlin <ahcarlin@gmail.com>
2018-10-09 18:52:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,9 +1,6 @@
 def sort_array_asc(arr)
-  count = 0 
   lowest = 100000000
   array = []
-  while count < arr.length do |a|
-    if a < lowest {a = lowest}
-      array << lowest
-    end
+  arr.each do |i, v|
+    if
 end 
